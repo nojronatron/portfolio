3b337f3e15ab7fba31037b960aa17b7c7d49ee40
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-04-15 21:56:58 -0700

    removed flags, added projects.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -29,28 +29,28 @@ class Footer extends Component {
                 }
                 style={{ display: "inline" }}
               >
-                <span
+                {/* <span
                   className="iconify language-icon mr-5"
                   data-icon="twemoji-flag-for-flag-united-kingdom"
                   data-inline="false"
                   id={window.$primaryLanguageIconId}
-                ></span>
+                ></span> */}
               </div>
               <div 
                 onClick={() =>
                   this.props.applyPickedLanguage(
-                    window.$secondaryLanguage,
+                    // window.$secondaryLanguage,
                     window.$primaryLanguageIconId
                   )
                 }
                 style={{ display: "inline" }}
               >
-                <span
+                {/* <span
                   className="iconify language-icon"
                   data-icon="twemoji-flag-for-flag-poland"
                   data-inline="false"
                   id={window.$secondaryLanguageIconId}
-                ></span>
+                ></span> */}
               </div>
             </div>
             <div className="col">
