4326aaa447d1e16e95c386399d1b1aba56788aa4
Jon Rumsey <Jon.Rumsey@gmail.com>
2023-04-02 15:50:26 -0700

    applied Prettier rules, manual test ui okay.

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
-import { Icon } from "@iconify/react";
-import nodejsIcon from "@iconify/icons-logos/nodejs";
-import reactIcon from "@iconify/icons-logos/react";
-import dotnetIcon from "@iconify/icons-logos/dotnet";
-import javaIcon from "@iconify/icons-logos/java";
+import React, { Component } from 'react';
+import { Icon } from '@iconify/react';
+import nodejsIcon from '@iconify/icons-logos/nodejs';
+import reactIcon from '@iconify/icons-logos/react';
+import dotnetIcon from '@iconify/icons-logos/dotnet';
+import javaIcon from '@iconify/icons-logos/java';
 
 class About extends Component {
   render() {
     if (this.props.sharedBasicInfo) {
-      var profilepic = "images/" + this.props.sharedBasicInfo.image;
+      var profilepic = 'images/' + this.props.sharedBasicInfo.image;
     }
     if (this.props.resumeBasicInfo) {
       var sectionName = this.props.resumeBasicInfo.section_name.about;
@@ -17,73 +17,73 @@ class About extends Component {
     }
 
     return (
-      <section id="about">
-        <div className="col-md-12">
-          <h1 style={{ color: "black" }}>
+      <section id='about'>
+        <div className='col-md-12'>
+          <h1 style={{ color: 'black' }}>
             <span>{sectionName}</span>
           </h1>
-          <div className="row center mx-auto mb-5">
-            <div className="col-md-4 mb-5">
+          <div className='row center mx-auto mb-5'>
+            <div className='col-md-4 mb-5'>
               <div>
-                  <img
-                    height="400px"
-                    width="400px"
-                    src={profilepic}
-                    alt="Jon hiking near Mt.Rainier in Washington State."
-                  />
+                <img
+                  height='400px'
+                  width='400px'
+                  src={profilepic}
+                  alt='Jon hiking near Mt.Rainier in Washington State.'
+                />
               </div>
-                <span style={{ cursor: "auto" }}>
-                  <Icon
-                    icon={nodejsIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
-                  />
-                  <Icon
-                    icon={reactIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
-                  />
-                  <Icon
-                    icon={dotnetIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
-                  />
-                  <Icon
-                    icon={javaIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
-                  />
-                </span>
+              <span style={{ cursor: 'auto' }}>
+                <Icon
+                  icon={nodejsIcon}
+                  style={{ fontSize: '400%', margin: '9% 5% 0 5%' }}
+                />
+                <Icon
+                  icon={reactIcon}
+                  style={{ fontSize: '400%', margin: '9% 5% 0 5%' }}
+                />
+                <Icon
+                  icon={dotnetIcon}
+                  style={{ fontSize: '400%', margin: '9% 5% 0 5%' }}
+                />
+                <Icon
+                  icon={javaIcon}
+                  style={{ fontSize: '400%', margin: '9% 5% 0 5%' }}
+                />
+              </span>
             </div>
 
-            <div className="col-md-8 center">
-              <div className="col-md-10">
-                <div className="card">
-                  <div className="card-header">
+            <div className='col-md-8 center'>
+              <div className='col-md-10'>
+                <div className='card'>
+                  <div className='card-header'>
                     <span
-                      className="iconify"
-                      data-icon="emojione:red-circle"
-                      data-inline="false"
-                    ></span>{" "}
-                    &nbsp;{" "}
+                      className='iconify'
+                      data-icon='emojione:red-circle'
+                      data-inline='false'
+                    ></span>{' '}
+                    &nbsp;{' '}
                     <span
-                      className="iconify"
-                      data-icon="twemoji:yellow-circle"
-                      data-inline="false"
-                    ></span>{" "}
-                    &nbsp;{" "}
+                      className='iconify'
+                      data-icon='twemoji:yellow-circle'
+                      data-inline='false'
+                    ></span>{' '}
+                    &nbsp;{' '}
                     <span
-                      className="iconify"
-                      data-icon="twemoji:green-circle"
-                      data-inline="false"
+                      className='iconify'
+                      data-icon='twemoji:green-circle'
+                      data-inline='false'
                     ></span>
                   </div>
                   <div
-                    className="card-body font-trebuchet text-justify ml-3 mr-3"
+                    className='card-body font-trebuchet text-justify ml-3 mr-3'
                     style={{
-                      height: "auto",
-                      fontSize: "132%",
-                      lineHeight: "200%",
+                      height: 'auto',
+                      fontSize: '132%',
+                      lineHeight: '200%',
                     }}
                   >
                     <br />
-                    <span className="wave">{hello} :) </span>
+                    <span className='wave'>{hello} :) </span>
                     <br />
                     <br />
                     {about}
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Footer extends Component {
   render() {
     if (this.props.sharedBasicInfo) {
       var networks = this.props.sharedBasicInfo.social.map(function (network) {
         return (
-          <span key={network.name} className="m-4">
-            <a href={network.url} target="_blank" rel="noopener noreferrer">
+          <span key={network.name} className='m-4'>
+            <a href={network.url} target='_blank' rel='noopener noreferrer'>
               <i className={network.class}></i>
             </a>
           </span>
@@ -16,35 +16,35 @@ class Footer extends Component {
 
     return (
       <footer>
-        <div className="container">
-          <div className="row">
-            <div className="col social-links">{networks}</div>
-            <div className="col">
-              <div 
+        <div className='container'>
+          <div className='row'>
+            <div className='col social-links'>{networks}</div>
+            <div className='col'>
+              <div
                 onClick={() =>
                   this.props.applyPickedLanguage(
                     window.$primaryLanguage,
                     window.$secondaryLanguageIconId
                   )
                 }
-                style={{ display: "inline" }}
+                style={{ display: 'inline' }}
               >
                 <span
-                  className="iconify language-icon mr-5"
-                  data-icon="twemoji-flag-for-flag-united-states"
-                  data-inline="false"
+                  className='iconify language-icon mr-5'
+                  data-icon='twemoji-flag-for-flag-united-states'
+                  data-inline='false'
                   id={window.$primaryLanguageIconId}
                 ></span>
               </div>
             </div>
-            <div className="col">
-              <div className="copyright py-4 text-center">
-                <div className="container">
+            <div className='col'>
+              <div className='copyright py-4 text-center'>
+                <div className='container'>
                   <small>
-                    Revision {" Feb 2023 - "}
+                    Revision {' Feb 2023 - '}
                     {this.props.sharedBasicInfo
                       ? this.props.sharedBasicInfo.name
-                      : "???"}
+                      : '???'}
                   </small>
                 </div>
               </div>
