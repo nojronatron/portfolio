2d1aa29a477b9899f93aed8386289822d586fd04
Jon Rumsey <Jon.Rumsey@gmail.com>
2023-04-02 13:47:58 -0700

    forced Prettier reformat on many files.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import ProjectDetailsModal from "./ProjectDetailsModal";
+import React, { Component } from 'react';
+import ProjectDetailsModal from './ProjectDetailsModal';
 
 class Projects extends Component {
   constructor(props) {
@@ -21,22 +21,26 @@ class Projects extends Component {
       var projects = this.props.resumeProjects.map(function (projects) {
         return (
           <div
-            className="col-sm-12 col-md-6 col-lg-4"
+            className='col-sm-12 col-md-6 col-lg-4'
             key={projects.title}
-            style={{ cursor: "pointer" }}
+            style={{ cursor: 'pointer' }}
           >
-            <span className="portfolio-item d-block">
-              <div className="foto" onClick={() => detailsModalShow(projects)}>
+            <span className='portfolio-item d-block'>
+              <div className='foto' onClick={() => detailsModalShow(projects)}>
                 <div>
                   <img
                     src={projects.images[0]}
-                    alt="projectImages"
-                    height="230"
-                    style={{marginBottom: 0, paddingBottom: 0, position: 'relative'}}
+                    alt='projectImages'
+                    height='230'
+                    style={{
+                      marginBottom: 0,
+                      paddingBottom: 0,
+                      position: 'relative',
+                    }}
                   />
-                  <span className="project-date">{projects.startDate}</span>
+                  <span className='project-date'>{projects.startDate}</span>
                   <br />
-                  <p className="project-title-settings mt-3">
+                  <p className='project-title-settings mt-3'>
                     {projects.title}
                   </p>
                 </div>
@@ -48,13 +52,13 @@ class Projects extends Component {
     }
 
     return (
-      <section id="portfolio">
-        <div className="col-md-12">
-          <h1 className="section-title" style={{ color: "black" }}>
+      <section id='portfolio'>
+        <div className='col-md-12'>
+          <h1 className='section-title' style={{ color: 'black' }}>
             <span>{sectionName}</span>
           </h1>
-          <div className="col-md-12 mx-auto">
-            <div className="row mx-auto">{projects}</div>
+          <div className='col-md-12 mx-auto'>
+            <div className='row mx-auto'>{projects}</div>
           </div>
           <ProjectDetailsModal
             show={this.state.detailsModalShow}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./index.scss";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.scss';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 /* GLOBAL VARIABLES */
 
-window.$primaryLanguage = "en";
-window.$primaryLanguageIconId = "primary-lang-icon";
+window.$primaryLanguage = 'en';
+window.$primaryLanguageIconId = 'primary-lang-icon';
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
 serviceWorker.register();
