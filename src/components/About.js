a564003843b6217a7db3005d53606db52f56c056
Jon Rumsey <jon.rumsey@gmail.com>
2023-05-25 16:05:38 -0700

    fixed image arrangement.

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,10 @@ class About extends Component {
     if (this.props.resumeBasicInfo) {
       var sectionName = this.props.resumeBasicInfo.section_name.about;
       var hello = this.props.resumeBasicInfo.description_header;
-      var about = this.props.resumeBasicInfo.description;
+      // var about = this.props.resumeBasicInfo.description;
+      var about = this.props.resumeBasicInfo.description.map((element) => {
+        return <div>{element}</div>;
+      });
     }
 
     return (
@@ -22,72 +25,51 @@ class About extends Component {
           <h1 style={{ color: 'black' }}>
             <span>{sectionName}</span>
           </h1>
-          <div className='row center mx-auto mb-5'>
-            <div className='col-md-4 mb-5'>
-              <div>
-                <img
-                  height='400px'
-                  width='400px'
-                  src={profilepic}
-                  alt='Jon hiking near Mt.Rainier in Washington State.'
-                />
-              </div>
-              <span style={{ cursor: 'auto' }}>
-                <Icon
-                  icon={nodejsIcon}
-                  style={{ fontSize: '400%', margin: '9% 5% 0 5%' }}
-                />
-                <Icon
-                  icon={reactIcon}
-                  style={{ fontSize: '400%', margin: '9% 5% 0 5%' }}
-                />
-                <Icon
-                  icon={dotnetIcon}
-                  style={{ fontSize: '400%', margin: '9% 5% 0 5%' }}
-                />
-                <Icon
-                  icon={javaIcon}
-                  style={{ fontSize: '400%', margin: '9% 5% 0 5%' }}
-                />
-              </span>
-            </div>
-
-            <div className='col-md-8 center'>
-              <div className='col-md-10'>
-                <div className='card'>
-                  <div className='card-header'>
-                    <span
-                      className='iconify'
-                      data-icon='emojione:red-circle'
-                      data-inline='false'
-                    ></span>{' '}
-                    &nbsp;{' '}
-                    <span
-                      className='iconify'
-                      data-icon='twemoji:yellow-circle'
-                      data-inline='false'
-                    ></span>{' '}
-                    &nbsp;{' '}
-                    <span
-                      className='iconify'
-                      data-icon='twemoji:green-circle'
-                      data-inline='false'
-                    ></span>
-                  </div>
-                  <div
-                    className='card-body font-trebuchet text-justify ml-3 mr-3'
+          <div className='row mx-auto mb-5'>
+            <div className='col-md-12 center'>
+              <div className='card'>
+                <div className='card-header'>
+                  <span
+                    className='iconify'
+                    data-icon='emojione:red-circle'
+                    data-inline='false'
+                  ></span>{' '}
+                  &nbsp;{' '}
+                  <span
+                    className='iconify'
+                    data-icon='twemoji:yellow-circle'
+                    data-inline='false'
+                  ></span>{' '}
+                  &nbsp;{' '}
+                  <span
+                    className='iconify'
+                    data-icon='twemoji:green-circle'
+                    data-inline='false'
+                  ></span>
+                </div>
+                <div
+                  className='card-body font-trebuchet text-justify ml-3 mr-3 clearfix'
+                  style={{
+                    height: 'auto',
+                    fontSize: '132%',
+                    lineHeight: '200%',
+                  }}
+                >
+                  <br />
+                  <span className='wave'>{hello} :) </span>
+                  <br />
+                  <br />
+                  <img
+                    width='360px'
+                    src={profilepic}
+                    alt='Image of Jon hiking near Mt.Rainier in Washington State, USA.'
+                    className='col-md-4 float-md-end mb-3 ms-md-3 rounded-4'
                     style={{
-                      height: 'auto',
-                      fontSize: '132%',
-                      lineHeight: '200%',
+                      boxShadow: '8px 10px 5px 0px #a59753',
                     }}
-                  >
-                    <br />
-                    <span className='wave'>{hello} :) </span>
-                    <br />
-                    <br />
-                    {about}
-                  </div>
+                  />
+
+                  {about}
                 </div>
               </div>
             </div>
