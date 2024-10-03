ec45c70962d811b94741d0f352f77f96e7f35541
Jon Rumsey <jonrumsey.dev@gmail.com>
2024-10-03 16:20:53 -0700

    refactor to functional component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,58 +1,32 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 
-class Footer extends Component {
-  static propTypes = {
-    sharedBasicInfo: PropTypes.object,
-    applyPickedLanguage: PropTypes.func,
-  };
+export default function Footer({ sharedBasicInfo }) {
+  const revision = '3 October 2024';
 
-  render() {
-    if (this.props.sharedBasicInfo) {
-      var networks = this.props.sharedBasicInfo.social.map(function (network) {
-        return (
-          <span key={network.name} className='m-4'>
-            <a href={network.url} target='_blank' rel='noopener noreferrer'>
-              <i className={network.class}></i>
-            </a>
-          </span>
-        );
-      });
-    }
+  if (sharedBasicInfo) {
+    var networks = sharedBasicInfo.social.map(function (network) {
+      return (
+        <span key={network.name} className='m-4'>
+          <a href={network.url} target='_blank' rel='noopener noreferrer'>
+            <i className={network.class}></i>
+          </a>
+        </span>
+      );
+    });
 
     return (
       <footer>
-        <div className='container'>
-          <div className='row'>
-            <div className='col social-links'>{networks}</div>
-            <div className='col'>
-              <div
-                onClick={() =>
-                  this.props.applyPickedLanguage(
-                    window.$primaryLanguage,
-                    window.$secondaryLanguageIconId
-                  )
-                }
-                style={{ display: 'inline' }}
-              >
-                <span
-                  className='iconify language-icon mr-5'
-                  data-icon='twemoji-flag-for-flag-united-states'
-                  data-inline='false'
-                  id={window.$primaryLanguageIconId}
-                ></span>
-              </div>
-            </div>
-            <div className='col'>
-              <div className='copyright py-3 text-center'>
-                <div className='container'>
-                  Revision {' 2 October 2024 - '}
-                  {this.props.sharedBasicInfo
-                    ? this.props.sharedBasicInfo.name
-                    : '???'}
-                </div>
-              </div>
-            </div>
+        <div className='d-flex flex-row justify-content-around'>
+          <div>{networks}</div>
+          <img
+            src='images/jrsoftware-logo-128x128.png'
+            alt='JR Software'
+            height='20em'
+          />
+          <div>
+            Revision {revision}
+            {', '}
+            {sharedBasicInfo ? sharedBasicInfo.name : '???'}
           </div>
         </div>
       </footer>
@@ -60,4 +34,6 @@ class Footer extends Component {
   }
 }
 
-export default Footer;
+Footer.propTypes = {
+  sharedBasicInfo: PropTypes.object,
+};
