42f2bc42f5de55b655e3fb9e0866482ea6a5ec01
Jon Rumsey <jonrumsey.dev@gmail.com>
2024-10-12 11:50:17 -0700

    cleanup experience layout, style

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -35,58 +35,38 @@ export default function Experience({ resumeExperience, resumeBasicInfo }) {
             background: '#AE944F',
             color: '#fff',
             textAlign: 'center',
+            fontSize: '2em',
           }}
           icon={<i className='fab fa-angular experience-icon'></i>}
           key={i}
         >
-          <div style={{ textAlign: 'left', marginBottom: '4px' }}>
-            {mainTech}
-          </div>
+          <div>{mainTech}</div>
 
-          <h3
-            className='vertical-timeline-element-title'
-            style={{ textAlign: 'left' }}
-          >
-            {work.title}
-          </h3>
-          <h4
-            className='vertical-timeline-element-subtitle'
-            style={{ textAlign: 'left' }}
-          >
-            {work.company}
-          </h4>
-          <div style={{ textAlign: 'left', marginTop: '15px' }}>{tech}</div>
+          <h3 className='vertical-timeline-element-title'>{work.title}</h3>
+          <h4 className='vertical-timeline-element-subtitle'>{work.company}</h4>
+          <div>{tech}</div>
         </VerticalTimelineElement>
       );
     });
   }
 
   return (
-    <section id='resume' className='pb-5'>
-      <div className='col-md-12 mx-auto'>
-        <div className='col-md-12'>
-          <h1 className='section-title' style={{ color: 'black' }}>
-            <span className='text-black' style={{ textAlign: 'center' }}>
-              {sectionName}
-            </span>
-          </h1>
-        </div>
-      </div>
-      <div className='col-md-8 mx-auto'>
-        <VerticalTimeline>
-          {work}
-          <VerticalTimelineElement
-            iconStyle={{
-              background: '#AE944F',
-              color: '#fff',
-              textAlign: 'center',
-            }}
-            icon={
-              <i className='fas fa-hourglass-start mx-auto experience-icon'></i>
-            }
-          />
-        </VerticalTimeline>
-      </div>
+    <section id='resume'>
+      <h1 className='section-title text-black'>{sectionName} </h1>
+      <VerticalTimeline>
+        {work}
+        <VerticalTimelineElement
+          iconStyle={{
+            background: '#AE944F',
+            color: '#fff',
+            textAlign: 'center',
+            fontSize: '1.8em',
+          }}
+          icon={
+            <div className='fas fa-hourglass-start mx-auto experience-icon'></div>
+          }
+        />
+      </VerticalTimeline>
     </section>
   );
 }
