7fd982f1397c0deec6e1b367e4caeabdc6442ef5
Jon Rumsey <jonrumsey.dev@gmail.com>
2024-10-13 11:50:56 -0700

    Refactor About to use get code icons function

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,119 +1,42 @@
-import { Icon } from '@iconify/react';
-import nodejsIcon from '@iconify/icons-logos/nodejs';
-import reactIcon from '@iconify/icons-logos/react';
-import dotnetIcon from '@iconify/icons-logos/dotnet';
-import javaIcon from '@iconify/icons-logos/java';
-import html5Icon from '@iconify/icons-logos/html-5';
-import css3Icon from '@iconify/icons-logos/css-3';
-import intellijIcon from '@iconify/icons-logos/intellij-idea';
-import vscodeIcon from '@iconify/icons-logos/visual-studio-code';
+import GetCodeLanguageIcons from './GetCodeLanguageIcons';
 import PropTypes from 'prop-types';
 
-export default function About({ resumeBasicInfo, sharedBasicInfo }) {
-  if (sharedBasicInfo) {
-    var profilepic = 'images/' + sharedBasicInfo.image;
-  }
-
-  if (resumeBasicInfo) {
+export default function About({ resumeBasicInfo, portfolioData }) {
+  if (resumeBasicInfo !== undefined) {
     var sectionName = resumeBasicInfo.section_name.about;
     var hello = resumeBasicInfo.description_header;
     var about = resumeBasicInfo.description.map((element, idx) => {
       return <div key={idx}>{element}</div>;
     });
 
+    if (portfolioData !== undefined) {
+      var profilepic = 'images/' + portfolioData.basic_info.image;
+      var codingLanguages = portfolioData.skills.codingLanguages;
+      var codingIcons = GetCodeLanguageIcons({ codingLanguages });
+    }
+
     return (
       <section id='about'>
-        <div className='col-md-12'>
-          <div className='row mx-auto m-2'>
-            <div className='col-md-12 center'>
-              <div className='card rounded-4'>
-                <div className='card-header rounded-top-4'>
-                  <span
-                    className='iconify'
-                    data-icon='emojione:red-circle'
-                    data-inline='false'
-                  ></span>{' '}
-                  &nbsp;{' '}
-                  <span
-                    className='iconify'
-                    data-icon='twemoji:yellow-circle'
-                    data-inline='false'
-                  ></span>{' '}
-                  &nbsp;{' '}
-                  <span
-                    className='iconify'
-                    data-icon='twemoji:green-circle'
-                    data-inline='false'
-                  ></span>
-                  <h1>{sectionName}</h1>
-                </div>
-                <div
-                  className='card-body font-trebuchet text-justify ml-3 mr-3 clearfix'
-                  style={{
-                    height: 'auto',
-                    fontSize: '132%',
-                    lineHeight: '200%',
-                  }}
-                >
-                  <br />
-                  <span className='wave'>{hello} :) </span>
-                  <br />
-                  <br />
-                  <img
-                    width='360px'
-                    src={profilepic}
-                    alt='Jon hiking near Mt.Rainier in Washington State, USA.'
-                    className='col-md-4 float-md-end mb-3 ms-md-3 rounded-4 profile-pic'
-                  />
+        <div className='card rounded-4'>
+          <div className='card-header rounded-top-4'>
+            <h1>{sectionName}</h1>
+          </div>
+          <div className='card-body text-justify '>
+            <br />
+            <span className='wave'>{hello} :) </span>
+            <br />
+            <br />
+            <img
+              width='300px'
+              src={profilepic}
+              alt='Jon hiking near Mt.Rainier in Washington State, USA.'
+              className='col-md-4 float-md-end mb-3 ms-md-3 rounded-4 profile-pic'
+            />
 
-                  {about}
-                </div>
-                <div className='card-footer d-flex justify-content-center'>
-                  <span style={{ cursor: 'auto' }}>
-                    <Icon
-                      className='mx-4 my-2'
-                      icon={vscodeIcon}
-                      style={{ fontSize: '300%' }}
-                    />
-                    <Icon
-                      className='mx-4 my-2'
-                      icon={reactIcon}
-                      style={{ fontSize: '300%' }}
-                    />
-                    <Icon
-                      className='mx-4 my-2'
-                      icon={html5Icon}
-                      style={{ fontSize: '300%' }}
-                    />
-                    <Icon
-                      className='mx-4 my-2'
-                      icon={javaIcon}
-                      style={{ fontSize: '300%' }}
-                    />
-                    <Icon
-                      className='mx-4 my-2'
-                      icon={dotnetIcon}
-                      style={{ fontSize: '300%' }}
-                    />
-                    <Icon
-                      className='mx-4 my-2'
-                      icon={css3Icon}
-                      style={{ fontSize: '300%' }}
-                    />{' '}
-                    <Icon
-                      className='mx-4 my-2'
-                      icon={nodejsIcon}
-                      style={{ fontSize: '300%' }}
-                    />
-                    <Icon
-                      className='mx-4 my-2'
-                      icon={intellijIcon}
-                      style={{ fontSize: '300%' }}
-                    />{' '}
-                  </span>
-                </div>
-              </div>
-            </div>
+            {about}
+          </div>
+          <div className='card-footer d-flex justify-content-center'>
+            {codingIcons}
           </div>
         </div>
       </section>
@@ -122,6 +45,6 @@ export default function About({ resumeBasicInfo, sharedBasicInfo }) {
 }
 
 About.propTypes = {
-  sharedBasicInfo: PropTypes.object,
   resumeBasicInfo: PropTypes.object,
+  portfolioData: PropTypes.object,
 };
