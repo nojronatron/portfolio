8bb956841f57dcf974f134091748556f1b478727
Jon Rumsey <jonrumsey.dev@gmail.com>
2024-10-14 16:12:06 -0700

    Simplify VerticalTimeline. Fix map indices.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,21 +9,22 @@ import PropTypes from 'prop-types';
 export default function Experience({ resumeExperience, resumeBasicInfo }) {
   if (resumeExperience && resumeBasicInfo) {
     var sectionName = resumeBasicInfo.section_name.experience;
-    var work = resumeExperience.map(function (work, i) {
+
+    var work = resumeExperience.map(function (work, idx) {
       const technologies = work.technologies;
       const mainTechnologies = work.mainTech;
 
-      var mainTech = mainTechnologies.map((mainTechnology, j) => {
+      var mainTech = mainTechnologies.map((technology, jdx) => {
         return (
-          <Badge pill className='main-badge mr-2 mb-2' key={j}>
-            {mainTechnology}
+          <Badge pill className='main-badge mr-2 mb-2' key={jdx}>
+            {technology}
           </Badge>
         );
       });
 
-      var tech = technologies.map((technology, k) => {
+      var tech = technologies.map((technology, idx) => {
         return (
-          <Badge pill className='experience-badge mr-2 mb-2' key={k}>
+          <Badge pill className='experience-badge mr-2 mb-2' key={idx}>
             {technology}
           </Badge>
         );
@@ -37,38 +38,34 @@ export default function Experience({ resumeExperience, resumeBasicInfo }) {
             background: '#AE944F',
             color: '#fff',
             textAlign: 'center',
-            fontSize: '2em',
           }}
-          icon={<i className='fab fa-angular experience-icon'></i>}
-          key={i}
+          icon={<div className='fab fa-angular experience-icon'></div>}
+          key={idx}
         >
           <div>{mainTech}</div>
 
           <h3 className='vertical-timeline-element-title'>{work.title}</h3>
           <h4 className='vertical-timeline-element-subtitle'>{work.company}</h4>
-          <div>{tech}</div>
+          <div style={{ textAlign: 'left', marginTop: '15px' }}>{tech}</div>
         </VerticalTimelineElement>
       );
     });
   }
 
   return (
-    <section id='resume'>
-      <h1 className='section-title'>{sectionName} </h1>
-      <VerticalTimeline>
-        <div className='workExperience'>{work}</div>
-        <VerticalTimelineElement
-          iconStyle={{
-            background: '#AE944F',
-            color: '#fff',
-            textAlign: 'center',
-            fontSize: '1.8em',
-          }}
-          icon={
-            <div className='fas fa-hourglass-start mx-auto experience-icon'></div>
-          }
-        />
-      </VerticalTimeline>
+    <section id='resume' className='pb-5'>
+      <div className='col-md-12 mx-auto'>
+        <div className='col-md-12'>
+          <h1 className='section-title' style={{ color: 'black' }}>
+            <span className='text-black' style={{ textAlign: 'center' }}>
+              {sectionName}
+            </span>
+          </h1>
+        </div>
+      </div>
+      <div className='col-md-8 mx-auto'>
+        <VerticalTimeline>{work}</VerticalTimeline>
+      </div>
     </section>
   );
 }
